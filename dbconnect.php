0c66977ba52c22b8c71a8fd26ea9ee828d603eb9
宜志富紹太 <shotagishitomi@gmail.com>
2019-12-04 18:59:58 +0900

    動作確認

--- dbconnect.php
+++ dbconnect.php
@@ -2,12 +2,18 @@
 
 //DBに接続
 // MySQLのテンプレ
-$host = getenv("HOSTNAME"); //MySQLがインストールされてるコンピュータ
-$dbname = getenv("DBNAME"); //使用するDB
+// $host = getenv("HOSTNAME"); //MySQLがインストールされてるコンピュータ
+// $dbname = getenv("DBNAME"); //使用するDB
+// $charset = "utf8"; //文字コード
+// $user = getenv('USERNAME'); //MySQLにログインするユーザー名
+// $password = getenv('PASSWORD'); //ユーザーのパスワード
+
+$host = getenv("us-cdbr-iron-east-05.cleardb.net"); //MySQLがインストールされてるコンピュータ
+$dbname = getenv("heroku_3654984a1f3de2b"); //使用するDB
 $charset = "utf8"; //文字コード
-$user = getenv('USERNAME'); //MySQLにログインするユーザー名
-// $user = getenv('USERNAME');
-$password = getenv('PASSWORD'); //ユーザーのパスワード
+$user = getenv('bbda35e6df46d8'); //MySQLにログインするユーザー名
+$password = getenv('fd7cb580'); //ユーザーのパスワード
+
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION, //SQLでエラーが表示された場合、画面にエラーが出力される
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC, //DBから取得したデータを連想配列の形式で取得する
